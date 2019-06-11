982ab316a74d68179e8f5dd8936a25c2a2625410
Alyson Bittner <Aly@MacBook-Pro.local>
2019-06-11 18:29:38 -0400

    quandl file

--- app.py
+++ app.py
@@ -2,18 +2,16 @@ from flask import Flask, render_template, request, redirect
 import pandas as pd
 import bokeh.plotting as bk
 import bokeh.embed as bke
-import requests as rq
-import simplejson as json
-from datetime import datetime as dt
+import quandl as qd
+
+qd.ApiConfig.api_key = 'WWhbwTh6zX3RTBHsv6a-'
 
 def get_data(ticker, month, year):
-    req = rq.get('https://www.quandl.com/api/v3/datasets/WIKI/' + ticker + '/data.json?api_key=WWhbwTh6zX3RTBHsv6a-')
-    j = req.json()
-    df = pd.DataFrame(j['dataset_data']['data'], columns = j['dataset_data']['column_names'])
-    df['DateTime'] = pd.to_datetime(df['Date'], format = '%Y-%m-%d')
-    df['Month'] = df['DateTime'].map(lambda x: x.month)
-    df['Day'] = df['DateTime'].map(lambda x: x.day)
-    df['Year'] = df['DateTime'].map(lambda x: x.year)
+    df = qd.get('WIKI/' + ticker)
+    df['Date'] = df.index
+    df['Month'] = df['Date'].map(lambda x: x.month)
+    df['Day'] = df['Date'].map(lambda x: x.day)
+    df['Year'] = df['Date'].map(lambda x: x.year)
     df_yr = df[df['Year'] == int(year)]
     return df_yr[df_yr['Month'] == int(month)]
 
@@ -21,8 +19,8 @@ def plot_data(data, ticker):
     p = bk.figure(x_axis_type = 'datetime')
     p.xaxis.axis_label = 'Date'
     p.yaxis.axis_label = 'Price'
-    p.line(data['DateTime'], data['Open'], legend = ticker + ' Open', line_color="green")
-    p.line(data['DateTime'], data['Close'], legend = ticker + ' Close')
+    p.line(data['Date'], data['Open'], legend = ticker + ' Open', line_color="green")
+    p.line(data['Date'], data['Close'], legend = ticker + ' Close')
     return p
 
 
